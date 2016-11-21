e6eda08550a5bd2fa36cf45234f795cd10f62e42
Austin Mason <austinpmason@gmail.com>
2016-11-21 16:03:42 +0000

    refactor gameboard

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,12 +5,12 @@ describe Board do
   
   it { expect(board).to be_instance_of Board }
   
-  context "when a new gameboard is created" do
-    it "should have 8 rows" do
+  context 'when a new gameboard is created' do
+    it 'should have 8 rows' do
       expect(board.gameboard.length).to eql 8
     end
     
-    it "should have 8 columns" do
+    it 'should have 8 columns' do
       expect(board.gameboard[0].length).to eql 8
       expect(board.gameboard[7].length).to eql 8
     end
