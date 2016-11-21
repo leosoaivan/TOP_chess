47c91413903f9b7b936fa9c884e3c656136ff030
Austin Mason <austinpmason@gmail.com>
2016-11-21 15:25:01 +0000

    create gameboard

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,8 @@
 class Board
+  attr_accessor :gameboard
+  
+  def initialize
+    @gameboard = Array.new(8) { Array.new(8) }
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe Board do
+  subject(:board) { Board.new }
   
-  it { expect(subject). to be_instance_of Board }
+  it { expect(board).to be_instance_of Board }
+  
+  context "when a new gameboard is created" do
+    it "should have 8 rows" do
+      expect(board.gameboard.length).to eql 8
+    end
+    
+    it "should have 8 columns" do
+      expect(board.gameboard[0].length).to eql 8
+      expect(board.gameboard[7].length).to eql 8
+    end
+  end
 end
